b7ff0724e6cdbe6502af53938ab35424b2d0e90d
Rodrigo Lins <rlinsdev@gmail.com>
2021-12-09 09:21:26 -0300

    refresh token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,32 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
-import { LOGIN_URL } from "../../../lib/spotify"
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+
+async function refreshAccessToken(token) {
+  try {
+
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log('refreshed Token: '+ refreshedToken);
+
+    return{
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    }
+
+
+  } catch (error) { 
+    console.log(error);
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -42,5 +68,13 @@ export default NextAuth({
       console.log('Refreshing token');
       return await refreshAccessToken(token);
     },
+
+    async session({session, token}) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.userName;
+
+      return session;
+    }
   },
 });
