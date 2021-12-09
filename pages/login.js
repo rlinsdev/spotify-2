0b3541e4cc77bf98f0b3addd04ab66b2abd95737
Rodrigo Lins <rlinsdev@gmail.com>
2021-12-09 20:44:04 -0300

    Login and logout

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import { getProviders, signIn } from "next-auth/react";
+import React from 'react';
 
-function Login() {
+function Login({providers}) {
   return (
-    <div>
-      <h1> login page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https://i.imgur.com/fPuEa9V.png" alt="" />
+      {Object.values(providers).map((provider)=>(
+        <div key={provider.name}>
+          <button className="bg-[#18d860] text-white p-5 rounded-full"
+          onClick={() => signIn(provider.id, {callbackUrl: "/" })}>
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
 
-export default Login
+export default Login;
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers
+    }
+  }
+
+}
