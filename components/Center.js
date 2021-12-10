21a584aa55795a41dd62b145fa623114f5f57d6c
Rodrigo Lins <rlinsdev@gmail.com>
2021-12-09 22:10:29 -0300

    Center - Header

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,21 @@
+import { useSession } from "next-auth/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
+
 function Center() {
+const {data:session} = useSession();
+
   return (
     <div className="flex-grow text-white">
-      <h1>I'm Center!</h1>
+      <header>
+        <div className="flex items-center bg-green-500 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+          <img 
+            className="rounded-full w-10 h10" 
+            src={session?.user.image} alt="" />
+            <h2>{session?.user.name}</h2>
+            <ChevronDownIcon className="h-5 w-5" />
+        </div>
+      </header>
+      <section></section>
     </div>
   )
 }
