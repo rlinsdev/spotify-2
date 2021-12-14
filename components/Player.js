db280ad8918eda3bc3e74eaae1b6b1cdccedbafb
Rodrigo Lins <rlinsdev@gmail.com>
2021-12-14 08:59:24 -0300

    Song Info Custom Hook

---
+++ components/Player.js
@@ -0,0 +1,27 @@
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
+import useSpotify from "../hooks/useSpotify"
+
+function Player() {
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+
+  const songInfo = useSongInfo();
+
+  return (
+    // left
+    <div>
+
+      <img src={songInfo?.album.images?.[0]?.url} alt="" />
+
+    </div>
+  )
+}
+
+export default Player
