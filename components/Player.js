b081b7c6d76a9c96334a92c0a024fb2bb41375c1
Rodrigo Lins <rlinsdev@gmail.com>
2021-12-21 13:00:44 -0300

    Set Volume

--- components/Player.js
+++ components/Player.js
@@ -99,6 +99,13 @@ function Player() {
         // onClick={() => spotifyApi.skipToNext() }
         className="button"
       />
+      <ReplyIcon className="button" />
+      </div>
+      {/* right */}
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon onClick={() => volume > 0 && setVolume(volume - 10)} className="button" />
+        <input className="w-14 md:w-28" type="range" value={volume} onChange={e=>setVolume(e.target.value)} min={0} max={100} />
+        <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className="button" />
       </div>
     </div>
   );
